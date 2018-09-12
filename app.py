31d7bba756bd1dbbf062c2f6d05693d1fca3655e
Dominik Sachsenhofer <dominik.sachsenhofer@gmail.com>
2018-09-12 13:44:22 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import talib
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '0'
+
+
+@app.route('/health')
+def health():
+    return '0'
+
+
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=8080)
